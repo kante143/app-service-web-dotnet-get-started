9e2eccb6f030e58adcdd640a726fb164720ab488
rajkante <55310095+kante143@users.noreply.github.com>
2021-05-31 18:24:24 +0530

    Update HomeController.cs
    update HC

--- aspnet-get-started/Controllers/HomeController.cs
+++ aspnet-get-started/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace aspnet_get_started.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page is Modified.";
 
             return View();
         }
